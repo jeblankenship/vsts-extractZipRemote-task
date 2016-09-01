24531dc6e932c54807515ed20cd957382739f392
James Blankenship <jeblankenship@gmail.com>
2016-08-31 21:36:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# vsts-extractZipRemote-task
-TFS task to extract files from zip file on remote server.
+# Remove Duplicate File(s) Task
+Build task for VS Team Services that extracts zip file on remote computer to a folder a that computer..
+
+
+08/30/2016: This task is not ready for general public use yet.
---
+++ task/extract-zipRemote.ps1
@@ -0,0 +1,34 @@
+﻿[CmdletBinding()]
+param(
+    [string] $server,
+    [string] $zipFile,
+    [string] $destination
+)
+
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | % { Write-HOST "  ${_} = $($PSBoundParameters[$_])" }
+
+$command = {
+    param(
+        [string]$zipFile,
+        [string]$destination
+    )
+
+    Add-Type -assembly “system.io.compression.filesystem”
+
+    if(Test-Path $destination){
+        #delete existing content
+        Write-Verbose "Removing existing content from $destination"
+        Remove-Item $destination\* -Recurse -Force
+    }
+    Write-Verbose "Unziping file $zipFile to $destination"
+    [io.compression.zipfile]::ExtractToDirectory($zipFile, $destination)
+    Write-Verbose "Zip file extraction complete."
+}
+
+
+Invoke-Command -ComputerName $server $command -ArgumentList $zipFile,$destination
